42eef0b08d4a8c2c09a36a55d4de1ae871aad5a6
Zippy-Waluse <zippywaluse@gmail.com>
2024-05-30 21:50:26 +0300

    added file

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,33 @@
 fun main(args: Array<String>) {
-    ispalindrome("Waluse")
+//    ispalindrome("Waluse")
+    var product = Product("Pen",20.0,1)
+
 }
 
-fun ispalindrome(str: String): Boolean {
-    var start = 0
-    var end = str.length - 1
-    while (start < end) {
-        if (str[start] != str[end]) {
-            return false
-        }
-        start++
-        end--
+//fun ispalindrome(str: String): Boolean {
+//    var start = 0
+//    var end = str.length - 1
+//    while (start < end) {
+//        if (str[start] != str[end]) {
+//            return false
+//        }
+//        start++
+//        end--
+//    }
+//    return true
+//}
+
+
+class Product(val name: String, val price: Double, var quantity: Int) {
+    fun calculateTotalCost(): Double {
+        return price * quantity
     }
-    return true
 }
+
+
+
+
+
+
+
+
