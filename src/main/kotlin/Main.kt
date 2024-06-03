daf4d81154f32f6b95718c2086f46ec794d322a7
Zippy-Waluse <zippywaluse@gmail.com>
2024-06-03 20:15:10 +0300

    made changes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,17 @@
 fun main(args: Array<String>) {
 //    ispalindrome("Waluse")
-    var product = Product("Pen",20.0,1)
+//    var product = Product("Pen",20.0,1)
+    var calculator = Calculator()
+    var add = calculator.add(5,4)
+    println(add)
 
+    var subtract = calculator.subtract(3,1)
+    println(subtract)
+
+    var multiply = calculator.multiply(2,2)
+    println(multiply)
+
+    var divide = calculator.divide(4,2)
 }
 
 //fun ispalindrome(str: String): Boolean {
@@ -24,6 +34,26 @@ class Product(val name: String, val price: Double, var quantity: Int) {
     }
 }
 
+
+//Create a class Calculator with functions add(), subtract(), multiply(), and divide().
+class Calculator{
+    fun add(number:Int,secondNumber: Int):Int{
+        return number + secondNumber
+
+    }
+
+    fun subtract(number: Int,secondNumber: Int):Int{
+        return number-secondNumber
+    }
+
+    fun multiply(number: Int,secondNumber:Int):Int{
+        return number- secondNumber
+    }
+
+    fun divide(number: Int,secondNumber: Int):Int{
+         return number / secondNumber
+    }
+}
 
 
 
